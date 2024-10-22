e4437b84e10c7fff63e168ae614399c673e1486b
nishtarw <nishta.sr04@gmail.com>
2024-10-22 19:58:18 -0400

    0000

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-// server.js
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Use environment variable for port or default to 3000
+const port = process.env.PORT || 3000;
+
+// Listen on all network interfaces (0.0.0.0)
+const hostname = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
